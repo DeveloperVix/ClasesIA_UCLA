7167cf2dd585bfab9f3cb74785c55e4fe485d266
DeveloperVix <vicomisterio@hotmail.com>
2021-03-17 11:04:16 -0600

    Estado espera y rotacion
    Se añadió funcionalidad del estado de espera y rotación, este último hay que probarlo.

--- Agente_Torreta/Assets/Scripts/EstadoEspera.cs
+++ Agente_Torreta/Assets/Scripts/EstadoEspera.cs
@@ -6,8 +6,18 @@ public class EstadoEspera : InstruccionesFSM
 {
     public override void EnterState(TorretaEstatica torreta)
     {
-        //Inicia el estado de espera
         Debug.Log("Inicio Estado de espera!");
+        torreta.currentState = AgentState.Idle;
+        torreta.Coroutine(Wait(torreta));
+    }
+
+    IEnumerator Wait(TorretaEstatica agent)
+    {
+        Debug.Log("Inicio conteo");
+        yield return new WaitForSeconds(agent.timeIdle);
+        ///Se ejecuta despues de X segundos
+        Debug.Log("Cambio a estado de rotar!");
+        agent.TransitionToState(agent.rotateState);
     }
 
     public override void UpdateState(TorretaEstatica torreta)
